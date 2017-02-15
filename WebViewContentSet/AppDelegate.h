2b8be668acd1708ab41206b8ed6e4ffef59eedf8
rockyMni <fengjian0213@126.com>
2017-02-15 17:00:43 +0800

    Initial Commit

---
+++ WebViewContentSet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewContentSet
+//
+//  Created by rocky on 2017/2/15.
+//  Copyright © 2017年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewContentSet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WebViewContentSet
+//
+//  Created by rocky on 2017/2/15.
+//  Copyright © 2017年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
